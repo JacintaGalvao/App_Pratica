4fea66a2aab764be57bdaea10c2810acbb8b8734
JacintaGalvao <jacintafgalvao@gmail.com>
2025-12-15 12:00:00 -0100

    Update App.py

--- App.py
+++ App.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 #import numpy as np
 import pandas as pd
-import openpyx1
+import openpyxl
 #import matplotlib.pyplot as plt
 #import time
 #import re
