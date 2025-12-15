ba0a2cb9dfb834b15db455b5110e63243cfdd22c
JacintaGalvao <jacintafgalvao@gmail.com>
2025-12-15 11:15:23 -0100

    Update App.py

--- App.py
+++ App.py
@@ -7,7 +7,7 @@ from streamlit_option_menu import option_menu
 #import re
 #from dateline impot date 
 st.header("Introduzindo os Elementos do Streamlit")
-menu =  option_menu (menu_title ="Menu", 
+menu = option_menu (menu_title ="Menu", 
                      options=["inicio","Gráficos estaticos", "Gráficos denâmicos", "Widgets", "Formulário"],
                      icons=["house", "bar-chart", "bar-chart-line", "toggles", "bar-chart" ],
                      menu_icon="cast",
